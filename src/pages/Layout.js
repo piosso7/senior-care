d52175dd676b6753c0e1276188e49be150a58712
Piotr Sokołowski <piosso07@gmail.com>
2023-05-22 18:32:21 +0200

    nav bar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,26 +1,39 @@
 import { Outlet, Link } from "react-router-dom";
+import Logo from "../pictures/LogoCareHome.svg";
 
 const Layout = () => {
   return (
     <>
       <nav>
+        <img src={Logo} alt="Logo"></img>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link className="link" to="/">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about-us">About Us</Link>
+            <Link className="link" to="/about-us">
+              About Us
+            </Link>
           </li>
           <li>
-            <Link to="/services">Services</Link>
+            <Link className="link" to="/services">
+              Services
+            </Link>
           </li>
           <li>
-            <Link to="/amenities">Amenities</Link>
+            <Link className="link" to="/amenities">
+              Amenities
+            </Link>
           </li>
           <li>
-            <Link to="/cost-&-payment">Cost & Payment</Link>
+            <Link className="link" to="/cost-&-payment">
+              Cost & Payment
+            </Link>
           </li>
         </ul>
+        <button>Contact Us</button>
       </nav>
       <Outlet />
     </>
