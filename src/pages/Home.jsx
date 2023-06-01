aae420c3e767a34164536565ccc52032ffe776e7
Piotr Sokołowski <piosso07@gmail.com>
2023-06-01 17:54:35 +0200

    home add our services cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,25 @@ import Fotter from "./Footer";
 import CheckedIco from "../pictures/home/checked.svg";
 import homeAboutUs from "../pictures/home/homeAboutUs.png";
 
+import osCare from "../pictures/home/osCare.svg";
+import osNutrition from "../pictures/home/osNutrition.svg";
+import osNursing from "../pictures/home/osNursing.svg";
+import osStaff from "../pictures/home/osStaff.svg";
+
 function AboutUsListEl(props) {
   return (
     <div className="aboutUsListEl">
       <img src={CheckedIco} alt="checked" />
+      <p>{props.description}</p>
+    </div>
+  );
+}
+
+function OurServicesCard(props) {
+  return (
+    <div className="card">
+      <img src={props.img} alt="Our Services Card Logo" />
+      <h3>{props.title}</h3>
       <p>{props.description}</p>
     </div>
   );
@@ -42,6 +57,31 @@ const Home = () => {
             <AboutUsListEl description="Urna pulvinar porttitor dignissim congue pellentesque ac hac." />
             <AboutUsListEl description="Eu adipiscing massa ut proin mauris orci tincidunt ac in." />
           </div>
+        </div>
+      </section>
+      <section className="shortOurServices">
+        <h2>Our Services</h2>
+        <div className="carts">
+          <OurServicesCard
+            img={osCare}
+            title="Resident Care"
+            description="Lorem ipsum dolor sit amet consectetur. Augue non malesuada placerat faucibus nam purus sem. Urna pulvinar porttitor dignissim congue pellentesque ac hac. "
+          />
+          <OurServicesCard
+            img={osNutrition}
+            title="Elderly Nutrition"
+            description="Lorem ipsum dolor sit amet consectetur. Augue non malesuada placerat faucibus nam purus sem. Urna pulvinar porttitor dignissim congue pellentesque ac hac. "
+          />
+          <OurServicesCard
+            img={osNursing}
+            title="Skilled Nursing"
+            description="Lorem ipsum dolor sit amet consectetur. Augue non malesuada placerat faucibus nam purus sem. Urna pulvinar porttitor dignissim congue pellentesque ac hac. "
+          />
+          <OurServicesCard
+            img={osStaff}
+            title="Caring Staff"
+            description="Lorem ipsum dolor sit amet consectetur. Augue non malesuada placerat faucibus nam purus sem. Urna pulvinar porttitor dignissim congue pellentesque ac hac. "
+          />
         </div>
       </section>
 
