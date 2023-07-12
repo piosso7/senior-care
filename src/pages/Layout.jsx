2d1628aec98dbba88f416b51aedc1c98c91012d6
Piotr Sokołowski <piosso07@gmail.com>
2023-07-12 21:00:46 +0200

    mobile burger menu

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,40 +1,57 @@
 import { Outlet, NavLink } from "react-router-dom";
+import { useState } from "react";
+import Icon from "@mdi/react";
+import { mdiMenu, mdiClose } from "@mdi/js";
 
 import Logo from "../pictures/LogoCareHome.svg";
 
 const Layout = () => {
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = () => {
+    setIsActive((current) => !current);
+  };
   return (
     <>
-      <nav>
+      <nav className={isActive ? "menu active" : "menu"}>
         <img src={Logo} alt="Logo"></img>
         <ul>
           <li>
-            <NavLink className="link" to="/senior-care">
+            <NavLink onClick={handleClick} className="link" to="/senior-care">
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink className="link" to="/about-us">
+            <NavLink onClick={handleClick} className="link" to="/about-us">
               About Us
             </NavLink>
           </li>
           <li>
-            <NavLink className="link" to="/services">
+            <NavLink onClick={handleClick} className="link" to="/services">
               Services
             </NavLink>
           </li>
           <li>
-            <NavLink className="link" to="/amenities">
+            <NavLink onClick={handleClick} className="link" to="/amenities">
               Amenities
             </NavLink>
           </li>
           <li>
-            <NavLink className="link" to="/cost-&-payment">
+            <NavLink
+              onClick={handleClick}
+              className="link"
+              to="/cost-&-payment"
+            >
               Cost & Payment
             </NavLink>
           </li>
         </ul>
         <button>Contact Us</button>
+        <Icon
+          onClick={handleClick}
+          className="bars"
+          path={isActive ? mdiClose : mdiMenu}
+          size={2}
+        />
       </nav>
       <Outlet />
     </>
